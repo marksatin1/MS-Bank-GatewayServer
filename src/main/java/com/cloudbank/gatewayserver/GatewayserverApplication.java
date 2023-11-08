443a3d754ea21c403e892bf330ecc67d167791f4
Mark Satin <msatin2009@gmail.com>
2023-11-07 23:51:48 -0500

    added filter chain, special routing configuration

--- src/main/java/com/cloudbank/gatewayserver/GatewayserverApplication.java
+++ src/main/java/com/cloudbank/gatewayserver/GatewayserverApplication.java
@@ -2,6 +2,11 @@ package com.cloudbank.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -10,4 +15,26 @@ public class GatewayserverApplication {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator cloudbankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder
+				.routes()
+				.route(p -> p
+						.path("/cloudbank/accounts/**")
+						.filters(f -> f.rewritePath("/cloudbank/accounts/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://ACCOUNTS"))
+				.route(p -> p
+						.path("/cloudbank/loans/**")
+						.filters(f -> f.rewritePath("/cloudbank/loans/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://LOANS"))
+				.route(p -> p
+						.path("/cloudbank/cards/**")
+						.filters(f -> f.rewritePath("/cloudbank/cards/(?<segment>.*)","/${segment}")
+						.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://CARDS"))
+				.build();
+
+	}
 }
